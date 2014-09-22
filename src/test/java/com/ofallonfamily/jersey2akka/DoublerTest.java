9fd2c7909c95e7ae6c34c6ce5a94e917093106f6
pofallon <paul@ofallonfamily.com>
2014-09-21 20:56:04 -0400

    Added test

---
+++ src/test/java/com/ofallonfamily/jersey2akka/DoublerTest.java
@@ -0,0 +1,37 @@
+package com.ofallonfamily.jersey2akka;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.GenericType;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by paul on 9/21/14.
+ */
+public class DoublerTest extends JerseyTest {
+
+    protected Application configure() {
+        return new ExampleApplication();
+    }
+
+    protected void configureClient(ClientConfig clientConfig) {
+        clientConfig.register(new JacksonJsonProvider());
+    }
+
+    @Test
+    public void testWithTwo() {
+
+        HashMap<String,Integer> map = target("doubler").path("2")
+                .request().get(new GenericType<HashMap<String, Integer>>() {});
+
+        assertEquals(new Integer(4), map.get("results"));
+
+    }
+
+}
